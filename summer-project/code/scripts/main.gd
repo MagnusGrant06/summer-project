1da2e0cf1eb7695bef29cf1b9e1da2d12506fce8
MagnusGrant06 <magnusgrant11@gmail.com>
2026-06-07 10:52:37 +1200

    Added loading screen to hide slow loading of initial API calls to spotify

--- summer-project/code/scripts/main.gd
+++ summer-project/code/scripts/main.gd
@@ -1,9 +1,12 @@
 extends Node3D
 @onready var record_holder = $Shelf/Node3D
-
-#load music into individual records (currently has errors due to only 1 album being present)
+@onready var loading_screen : Sprite2D = $Sprite2D
+@onready var animation : AnimationPlayer = $AnimationPlayer
+#load music into individual display records
 func _ready() -> void:
-	setup_display_albums()
+	show_logo()
+	await setup_display_albums()
+	hide_logo()
 
 
 func setup_display_albums():
@@ -14,3 +17,16 @@ func setup_display_albums():
 			continue
 		MusicManager.load_record_data(record, MusicManager.all_albums[i])
 		i+=1
+
+func show_logo():
+	var logo : Image = Image.load_from_file("res://assets/loading_screen.png")
+	logo.resize(get_viewport().get_visible_rect().size.x, get_viewport().get_visible_rect().size.y)
+	var logo_text : ImageTexture = ImageTexture.create_from_image(logo)
+	loading_screen.position = get_viewport().get_visible_rect().get_center()
+	loading_screen.texture = logo_text
+	
+
+func hide_logo():
+	animation.play("loading_screen_fade")
+	await get_tree().create_timer(2.0).timeout
+	loading_screen.queue_free()
